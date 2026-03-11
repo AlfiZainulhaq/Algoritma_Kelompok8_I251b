3311dec0ed39ca5a11070f8b853b5b2c6d8c0145
alfi <alfizainulhaq@gmail.com>
2026-03-11 13:51:04 +0700

    refactor file

--- src/BangunRuang/BangunRuang.java
+++ src/BangunRuang/BangunRuang.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Algoritma;
+package BangunRuang;
 
 /**
  *
  * @author alfizainulhaq
  */
-public class Array2Dimensi {
+public class BangunRuang {
     
 }
